9370987a81d3ca79e447702ca840700eab48ef13
DylanJu <dylan@daangnpay.com>
2025-07-12 06:52:00 +0900

    config: 타입을 import 할 때 type을 명시하도록 설정

--- apps/frontend/.eslintrc.cjs
+++ apps/frontend/.eslintrc.cjs
@@ -7,5 +7,12 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      {
+        prefer: 'type-only', // 타입만 import하는 경우 import type을 사용하도록 강제
+        fixStyle: 'inline-type-imports', // 타입과 값을 함께 import할 때 개별 타입을 명시하도록 설정
+      },
+    ],
   },
 }
