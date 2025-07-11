6c1646d2136abe4d88ae4b1baa36ebdf41d4193e
DylanJu <dylan@daangnpay.com>
2025-07-12 08:57:01 +0900

    config(lint): 최대길이 130자 적용

--- apps/frontend/.eslintrc.cjs
+++ apps/frontend/.eslintrc.cjs
@@ -14,5 +14,18 @@ module.exports = {
         fixStyle: 'inline-type-imports', // 타입과 값을 함께 import할 때 개별 타입을 명시하도록 설정
       },
     ],
+    'max-len': [
+      'error',
+      {
+        code: 130, // 최대 줄 길이를 130자로 제한
+        tabWidth: 2, // 탭 너비를 2칸으로 설정
+        ignoreComments: true, // 주석은 길이 제한에서 제외
+        ignoreTrailingComments: true, // 줄 끝 주석은 길이 제한에서 제외
+        ignoreUrls: true, // URL은 길이 제한에서 제외
+        ignoreStrings: true, // 문자열은 길이 제한에서 제외
+        ignoreTemplateLiterals: true, // 템플릿 리터럴은 길이 제한에서 제외
+        ignoreRegExpLiterals: true, // 정규식은 길이 제한에서 제외
+      },
+    ],
   },
 }
