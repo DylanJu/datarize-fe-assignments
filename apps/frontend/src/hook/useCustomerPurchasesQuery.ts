42a502955dd28b3a9c4834ec72c8bc2e7cdc9b73
DylanJu <dylan@daangnpay.com>
2025-07-12 07:02:06 +0900

    feat: 고객 구매내역 query 추가

---
+++ apps/frontend/src/hook/useCustomerPurchasesQuery.ts
@@ -0,0 +1,7 @@
+import { useQuery } from '@tanstack/react-query'
+import { customerPurchasesQueryOptions } from './query-options/customerPurchasesQueryOptions'
+import type { CustomerPurchasesRequest } from '../api/type'
+
+export const useCustomerPurchasesQuery = (params: CustomerPurchasesRequest) => {
+  return useQuery(customerPurchasesQueryOptions(params))
+}
