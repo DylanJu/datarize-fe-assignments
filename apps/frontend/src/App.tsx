2bd75a9338a023bd828db060dc9a9a3131036df9
DylanJu <dylan@daangnpay.com>
2025-07-12 06:55:12 +0900

    feat: App에 대시보드 query 추가

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,4 +1,17 @@
-function App() {
+import type { FC } from 'react'
+
+import { useDashBoardQuery } from './hook/useDashBoardQuery'
+
+const App: FC = () => {
+  const [{ data: customers }, { data: purchaseFrequency }] = useDashBoardQuery({
+    customerPurchasesRequest: {
+      customerId: 1,
+    },
+  })
+
+  console.log(customers)
+  console.log(purchaseFrequency)
+
   return <div>hello world</div>
 }
 
