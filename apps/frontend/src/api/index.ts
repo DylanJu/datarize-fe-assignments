9f7b7444ffafcebdd09ec86670b6853b84cd3072
DylanJu <dylan@daangnpay.com>
2025-07-12 22:33:52 +0900

    feat: api 공통 에러핸들링을 위한 유틸함수 추가

--- apps/frontend/src/api/index.ts
+++ apps/frontend/src/api/index.ts
@@ -6,8 +6,18 @@ import type {
   CustomerPurchasesRequest,
   CustomerPurchasesResponse,
 } from './type'
+import { ApiError } from '../util/exception'
+import { API_ERROR_MESSAGE } from '../constant/message'
 
 const BASE_URL = 'http://localhost:4000'
+
+const _fetch = async (url: string) => {
+  const response = await fetch(url)
+  if (!response.ok) {
+    throw new ApiError(API_ERROR_MESSAGE)
+  }
+  return response.json()
+}
 
 export const fetchPurchaseFrequency = async (params?: PurchaseFrequencyRequest): Promise<PurchaseFrequencyResponse> => {
   const url = new URL(`${BASE_URL}/api/purchase-frequency`)
@@ -17,8 +27,9 @@ export const fetchPurchaseFrequency = async (params?: PurchaseFrequencyRequest):
     url.searchParams.append('to', params.to)
   }
 
-  const response = await fetch(url.toString())
-  return response.json()
+  const response = await _fetch(url.toString())
+
+  return response
 }
 
 export const fetchCustomers = async (params?: CustomersRequest): Promise<CustomersResponse> => {
@@ -26,11 +37,13 @@ export const fetchCustomers = async (params?: CustomersRequest): Promise<Custome
   if (params?.sortBy) url.searchParams.append('sortBy', params.sortBy)
   if (params?.name) url.searchParams.append('name', params.name)
 
-  const response = await fetch(url.toString())
-  return response.json()
+  const response = await _fetch(url.toString())
+
+  return response
 }
 
 export const fetchCustomerPurchases = async (params: CustomerPurchasesRequest): Promise<CustomerPurchasesResponse> => {
-  const response = await fetch(`${BASE_URL}/api/customers/${params.customerId}/purchases`)
-  return response.json()
+  const response = await _fetch(`${BASE_URL}/api/customers/${params.customerId}/purchases`)
+
+  return response
 }
---
+++ apps/frontend/src/constant/message.ts
@@ -0,0 +1,1 @@
+export const API_ERROR_MESSAGE = '알 수 없는 에러가 발생했어요. 잠시 후 다시 시도해주세요.'
